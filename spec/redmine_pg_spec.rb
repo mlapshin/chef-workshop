1df1f98ba1c49729c4a7afa376232f6f9036a9ed
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-12-04 12:34:28 +0400

    failing spec for pg recipe

---
+++ spec/redmine_pg_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "redmine::pg recipe" do
+  run_chef_on :vm0 do |chef|
+    chef.add_recipe "redmine::pg"
+  end
+
+  it "should install postgresql server" do
+    vm0.should listen_port(5455)
+    vm0.should have_user("postgres").in_group("postgres")
+
+    vm0.execute_as("postgres", "psql -A -t -d postgres -c 'select 1+1'").stdout.should == "2\n"
+  end
+end
