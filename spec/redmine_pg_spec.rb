b2484a8c6a4dea86d9c413598a87d2c706e4038a
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-12-04 13:16:08 +0400

    failing spec which tests incoming pg connections

--- spec/redmine_pg_spec.rb
+++ spec/redmine_pg_spec.rb
@@ -1,3 +1,4 @@
+ENV["SKIPROLLBACK"] = '1'
 require 'spec_helper'
 
 describe "redmine::pg recipe" do
@@ -5,10 +6,21 @@ describe "redmine::pg recipe" do
     chef.add_recipe "redmine::pg"
   end
 
-  it "should install postgresql server" do
-    vm0.should listen_port(5455)
-    vm0.should have_user("postgres").in_group("postgres")
+  run_chef_on :vm1 do |chef|
+    chef.add_recipe "postgresql::client"
+  end
 
-    vm0.execute_as("postgres", "psql -A -t -d postgres -c 'select 1+1'").stdout.should == "2"
+  let(:server) { vm0 }
+  let(:client) { vm1 }
+
+  it "should install postgresql server" do
+    server.should listen_port(5455)
+    server.should have_user("postgres").in_group("postgres")
+
+    server.execute_as("postgres", "psql -A -t -d postgres -c 'select 1+1'").stdout.should == "2"
+  end
+
+  it "should allow incoming connections" do
+    client.execute("PGPASSWORD=s3cure psql -A -t -d postgres -U postgres -p 5455 -h #{server.ip} -c 'select 1+1'").stdout.should == '2'
   end
 end
